c7f66e48ee0072f98fcc506abbe50cee8d355018
RedEscape <wachers511@gmail.com>
2021-05-13 22:13:44 +0300

    homework 2 exercises

--- src/example.ts
+++ src/example.ts
@@ -1,52 +1,27 @@
-//
-// enum MethodEnum {
-//     add,
-//     sub,
-//     div,
-// }
-//
-// function calculate(method: MethodEnum, first: number, second: number): number {
-//     switch (method) {
-//         case MethodEnum.add: return first + second
-//         case MethodEnum.sub: return first - second
-//         case MethodEnum.div: return first / second
-//     }
-// }
-//
-// calculate(MethodEnum.sub, 3,5)
-//
-// type TypeFn = () => number;
-// type TypeDefaultFn = () => void;
-//
-// const ArrowFunc: TypeDefaultFn = () => console.log('Hi')
-//
-// interface MyArray<T> {
-//     [n: number]: T;
-//
-//     map<U>(fn: (el: T) => U): U[];
-// }
-// const tsArr: Array<number> = [1,2,3,4]
-//
-// tsArr.map((i) => i +1);
-// tsArr.map((i) => `${i} +1`);
-//
-// function getLen<T extends Array<any>>(arr: T):number {return arr.length}
-//
-// getLen([1,2,3])
-
+//1
 const concat = (str1: string, str2: string): string => str1 + str2;
-
+//2
 interface myHometaskInterface {
   howIDoIt: string;
   simeArray: (string | number)[];
   withData: [{ howIDoIt: string; simeArray: [string, number] }];
 }
+//2 test
 const myHometask: myHometaskInterface = {
   howIDoIt: 'fast',
   simeArray: ['bla', 'blabla', 23],
   withData: [{ howIDoIt: 'slow', simeArray: ['eh', 12] }],
 };
-
-interface MyArray<T> {
+//3
+interface MyArrayInterface<T> {
   [N: number]: T;
+
+  reduce<U>(fn: (acc: U, curr: T, currIndex: number, array: T[]) => U, initialValue: U): U;
 }
+//3 test
+const testArray: MyArrayInterface<number> = [1,2,3,4]
+const test1 = testArray.reduce((acc , curr)=> acc += curr,0)
+const test2 = testArray.reduce((acc , curr, i)=> ({[acc] : curr}),{})
+
+const stringArray: MyArrayInterface<string> = ['a','b','c']
+const testString = testArray.reduce((acc , curr)=> `${acc} ${curr}`,``)
